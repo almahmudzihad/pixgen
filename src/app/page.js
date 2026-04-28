f082916be7beceaec59bc58ce461fbb598104202
almahmudzihad <amzihad@gmail.com>
2026-04-28 13:06:28 +0600

    top image load

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/Banner";
+import TopGenerations from "@/components/TopGenerations";
 
 
 export default function Home() {
   return (
     <div className="">
       <Banner />
+      <TopGenerations />
     </div>
   );
 }
---
+++ src/components/TopGenerations.jsx
@@ -0,0 +1,21 @@
+import PhotoCard from "./PhotoCard"
+
+
+const TopGenerations = async () => {
+    const res = await fetch('https://pixgen-teal.vercel.app/data.json')
+    const data = await res.json()
+    const topPhotos = data.slice(0, 8)
+    
+  return (
+    <div>
+        <h1 className="text-2xl font-bold my-5">Top Generations</h1>
+
+            <div className="grid grid-cols-4 gap-5">
+                {topPhotos.map(photo => <PhotoCard key={photo.id} photo={photo} />)}
+            </div>
+
+    </div>
+  )
+}
+
+export default TopGenerations
